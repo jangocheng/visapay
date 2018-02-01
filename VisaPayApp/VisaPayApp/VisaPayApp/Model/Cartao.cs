d4dfb99f3e446440a21e6886f5a7a63d3f054152
Rodrigo Paiva <rodrigojpaiva@hotmail.com.br>
2018-02-01 18:25:24 -0300

    Start Project

---
+++ VisaPayApp/VisaPayApp/VisaPayApp/Model/Cartao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisaPayApp.Model
+{
+    public class Cartao
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; }
+        public int DiaVencimento { get; set; }
+        public int AnoVencimento { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int CodigoSeguranca { get; set; }
+
+    }
+}
